f6ac542da112026622bb0c18d4f87ba4301e7bdd
Jason Humphries <humphriesj@gmail.com>
2017-11-21 17:50:57 -0500

    refactor MotionTracker out of view controller. add beginning file for logging data to JSON

--- ProximityAnalytics/Controllers/MotionViewController.swift
+++ ProximityAnalytics/Controllers/MotionViewController.swift
@@ -21,20 +21,17 @@ class MotionViewController: UIViewController {
         super.viewDidLoad()
     }
     
-    //MARK: utility
-    
-    func degrees(_ radians:Double) -> Double {
-        return radians * 180.0 / .pi
-    }
-    
-    
     //MARK: Actions
     
     @IBAction func motionSwitchAction(_ sender: UISwitch) {
         if sender.isOn {
-            self.startDeviceMotion()
+            MotionTracker.sharedInstance.startDeviceMotion(withUpdateHandler: { [weak self] (data: CMDeviceMotion?) in
+                if let deviceMotionData = data {
+                    self?.updateDeviceMotionLabels(deviceMotionData)
+                }
+            })
         } else {
-            self.stopDeviceMotion()
+            MotionTracker.sharedInstance.stopDeviceMotion()
         }
     }
     
@@ -51,14 +48,14 @@ class MotionViewController: UIViewController {
         let accelY = accelData.y.shortValue
         let accelZ = accelData.z.shortValue
         let rotation = atan2(accelData.x, accelData.y) - .pi
-        print("Accelerometer Update: [x:\(accelX), y:\(accelY), z:\(accelZ)], rotation: \(self.degrees(rotation).shortValue)")
-        self.accelerationLabel.text = "x: \(accelX)\ny:\(accelY)\naccelZ:\(accelZ)\nrotation:\(self.degrees(rotation).shortValue)"
+        print("Accelerometer Update: [x:\(accelX), y:\(accelY), z:\(accelZ)], rotation: \(MotionTracker.degrees(rotation).shortValue)")
+        self.accelerationLabel.text = "x: \(accelX)\ny:\(accelY)\naccelZ:\(accelZ)\nrotation:\(MotionTracker.degrees(rotation).shortValue)"
 
         // euler
         let attitude = deviceMotion.attitude
-        let roll = self.degrees(attitude.roll).shortValue
-        let pitch = self.degrees(attitude.pitch).shortValue
-        let yaw = self.degrees(attitude.yaw).shortValue
+        let roll = MotionTracker.degrees(attitude.roll).shortValue
+        let pitch = MotionTracker.degrees(attitude.pitch).shortValue
+        let yaw = MotionTracker.degrees(attitude.yaw).shortValue
         print("Roll: \(roll), Pitch: \(pitch), Yaw: \(yaw)")
         self.eulerLabel.text = "roll: \(roll)\npitch:\(pitch)\nyaw:\(yaw)"
 
@@ -80,154 +77,5 @@ class MotionViewController: UIViewController {
         print("MAG - x: \(magX), y: \(magY), z: \(magZ), accuracy: \(magAccString)")
         self.magneticLabel.text = "x: \(magX)\ny: \(magY)\nz: \(magZ)\naccuracy: \(magAccString)"
     }
-    
-    func startDeviceMotion() {
-        let motionMgr = MotionTracker.motionManager
-        if motionMgr.isDeviceMotionAvailable {
-            motionMgr.deviceMotionUpdateInterval = 1.0 / 3.0
-            // Using the gyroscope, Core Motion separates user movement from gravitational acceleration and presents each as its own property of the CMDeviceMotion instance
-            //motionMgr.startDeviceMotionUpdates(to: .main, withHandler: { [weak self] (data: CMDeviceMotion?, error) in
-            motionMgr.startDeviceMotionUpdates(using: CMAttitudeReferenceFrame.xArbitraryCorrectedZVertical, to: .main, withHandler: { [weak self] (data: CMDeviceMotion?, error) in
-                guard error == nil else {
-                    print("Error with DecviceMotion: \(error?.localizedDescription ?? "Unknown Error")")
-                    return
-                }
-                guard let deviceMotion = data else {
-                    print("Error with DecviceMotion: data is bad!")
-                    return
-                }
-                self?.updateDeviceMotionLabels(deviceMotion)
-            })
-        }
-    }
-    
-    func stopDeviceMotion() {
-        MotionTracker.motionManager.stopDeviceMotionUpdates()
-    }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //MARK: Accelerometer
-    
-    func updateAccelerationLabel(_ data: CMAccelerometerData) {
-        //
-    }
-    
-    func startAccelerometer() {
-        let motionMgr = MotionTracker.motionManager
-        if motionMgr.isAccelerometerAvailable {
-            motionMgr.accelerometerUpdateInterval = 1.0 / 250.0
-            motionMgr.startAccelerometerUpdates(to: .main, withHandler: { [weak self] (accelData: CMAccelerometerData?, error) in
-                if error != nil {
-                    print("Accelerometer Error: \(error?.localizedDescription ?? "Unknown Error")")
-                    return
-                }
-                guard let accelData = accelData else {
-                    print("Bad Acceleration Data!")
-                    return
-                }
-                let accelX = accelData.acceleration.x
-                let accelY = accelData.acceleration.y
-                let accelZ = accelData.acceleration.z
-                print("Accelerometer Update: [x:\(accelX), y:\(accelY), z:\(accelZ)]")
-                self?.updateAccelerationLabel(accelData)
-            })
-        }
-    }
-    
-    func stopAccelerometer() {
-        MotionTracker.motionManager.stopAccelerometerUpdates()
-    }
-    
-    
-    
-    
-    //MARK: Gyro
-    
-    func updateGyroLabel(_ data: CMGyroData) {
-        //
-    }
-    
-    func startGyro() {
-        let motionMgr = MotionTracker.motionManager
-        if motionMgr.isGyroAvailable {
-            motionMgr.gyroUpdateInterval = 1.0 / 30.0
-            motionMgr.startGyroUpdates(to: .main, withHandler: { [weak self] (data: CMGyroData?, error) in
-                if error != nil {
-                    print("Gyro Error: \(error?.localizedDescription ?? "Unknown Error")")
-                    return
-                }
-                guard let gyroData = data else {
-                    print("Bad Gyro Data!")
-                    return
-                }
-                let gyroX = gyroData.rotationRate.x
-                let gyroY = gyroData.rotationRate.y
-                let gyroZ = gyroData.rotationRate.z
-                print("Gyro Update: [x:\(gyroX), y:\(gyroY), z:\(gyroZ)]")
-                self?.updateGyroLabel(gyroData)
-            })
-        }
-    }
-    
-    func stopGyro() {
-        MotionTracker.motionManager.stopGyroUpdates()
-    }
-    
-    
-    
-    
-    //MARK: Magnetometer
-    
-    func updateMagnetometerLabel(_ data: CMMagnetometerData) {
-        //
-    }
-    
-    func startMagnetometer() {
-        let motionMgr = MotionTracker.motionManager
-        if motionMgr.isMagnetometerAvailable {
-            motionMgr.magnetometerUpdateInterval = 1.0 / 30.0
-            motionMgr.startMagnetometerUpdates(to: OperationQueue.main) { [weak self] (data: CMMagnetometerData?, error) in
-                // data is CMMagnetometerData?
-                if error != nil {
-                    print("Magnetometer Error: \(error?.localizedDescription ?? "Unknown Error")")
-                    return
-                }
-                guard let magnetometerData = data else {
-                    print("Bad Magnetometer Data!")
-                    return
-                }
-                let magX = magnetometerData.magneticField.x
-                let magY = magnetometerData.magneticField.y
-                let magZ = magnetometerData.magneticField.z
-                print("Magnetometer Update: [x:\(magX), y:\(magY), z:\(magZ)]")
-                self?.updateMagnetometerLabel(magnetometerData)
-            }
-
-        }
-    }
-    
-    func stopMagnetometer() {
-        MotionTracker.motionManager.stopMagnetometerUpdates()
-    }
-    
-
-}
-
-
-extension Double {
-    var shortValue: String {
-        return String(format: "%.4f", self)
-    }
 }
--- ProximityAnalytics/Helper/MotionTracker.swift
+++ ProximityAnalytics/Helper/MotionTracker.swift
@@ -19,4 +19,158 @@ class MotionTracker: NSObject {
        return CMMotionManager()
     }()
     
+    
+    
+    func startDeviceMotion(withUpdateHandler updateHandler: @escaping (CMDeviceMotion?) -> ()) {
+        let motionMgr = MotionTracker.motionManager
+        if motionMgr.isDeviceMotionAvailable {
+            motionMgr.deviceMotionUpdateInterval = 1.0 / 3.0
+            // Using the gyroscope, Core Motion separates user movement from gravitational acceleration and presents each as its own property of the CMDeviceMotion instance
+            //motionMgr.startDeviceMotionUpdates(to: .main, withHandler: { [weak self] (data: CMDeviceMotion?, error) in
+            motionMgr.startDeviceMotionUpdates(using: CMAttitudeReferenceFrame.xArbitraryCorrectedZVertical, to: .main, withHandler: { (data: CMDeviceMotion?, error) in
+                guard error == nil else {
+                    print("Error with DecviceMotion: \(error?.localizedDescription ?? "Unknown Error")")
+                    updateHandler(nil)
+                    return
+                }
+                guard let deviceMotion = data else {
+                    print("Error with DecviceMotion: data is bad!")
+                    updateHandler(nil)
+                    return
+                }
+                updateHandler(deviceMotion)
+            })
+        }
+    }
+    
+    func stopDeviceMotion() {
+        MotionTracker.motionManager.stopDeviceMotionUpdates()
+    }
+    
+    //MARK: utility
+    
+    static func degrees(_ radians:Double) -> Double {
+        return radians * 180.0 / .pi
+    }
+
+    
+    
+    
+    
+    //////
+    // MARK: RAW DATA - Not using because DeviceMotion has a better representation for the purposes of this app.
+    
+    
+    //MARK: Accelerometer
+    
+    func updateAccelerationLabel(_ data: CMAccelerometerData) {
+        //
+    }
+    
+    func startAccelerometer() {
+        let motionMgr = MotionTracker.motionManager
+        if motionMgr.isAccelerometerAvailable {
+            motionMgr.accelerometerUpdateInterval = 1.0 / 250.0
+            motionMgr.startAccelerometerUpdates(to: .main, withHandler: { [weak self] (accelData: CMAccelerometerData?, error) in
+                if error != nil {
+                    print("Accelerometer Error: \(error?.localizedDescription ?? "Unknown Error")")
+                    return
+                }
+                guard let accelData = accelData else {
+                    print("Bad Acceleration Data!")
+                    return
+                }
+                let accelX = accelData.acceleration.x
+                let accelY = accelData.acceleration.y
+                let accelZ = accelData.acceleration.z
+                print("Accelerometer Update: [x:\(accelX), y:\(accelY), z:\(accelZ)]")
+                self?.updateAccelerationLabel(accelData)
+            })
+        }
+    }
+    
+    func stopAccelerometer() {
+        MotionTracker.motionManager.stopAccelerometerUpdates()
+    }
+    
+    
+    
+    
+    //MARK: Gyro
+    
+    func updateGyroLabel(_ data: CMGyroData) {
+        //
+    }
+    
+    func startGyro() {
+        let motionMgr = MotionTracker.motionManager
+        if motionMgr.isGyroAvailable {
+            motionMgr.gyroUpdateInterval = 1.0 / 30.0
+            motionMgr.startGyroUpdates(to: .main, withHandler: { [weak self] (data: CMGyroData?, error) in
+                if error != nil {
+                    print("Gyro Error: \(error?.localizedDescription ?? "Unknown Error")")
+                    return
+                }
+                guard let gyroData = data else {
+                    print("Bad Gyro Data!")
+                    return
+                }
+                let gyroX = gyroData.rotationRate.x
+                let gyroY = gyroData.rotationRate.y
+                let gyroZ = gyroData.rotationRate.z
+                print("Gyro Update: [x:\(gyroX), y:\(gyroY), z:\(gyroZ)]")
+                self?.updateGyroLabel(gyroData)
+            })
+        }
+    }
+    
+    func stopGyro() {
+        MotionTracker.motionManager.stopGyroUpdates()
+    }
+    
+    
+    
+    
+    //MARK: Magnetometer
+    
+    func updateMagnetometerLabel(_ data: CMMagnetometerData) {
+        //
+    }
+    
+    func startMagnetometer() {
+        let motionMgr = MotionTracker.motionManager
+        if motionMgr.isMagnetometerAvailable {
+            motionMgr.magnetometerUpdateInterval = 1.0 / 30.0
+            motionMgr.startMagnetometerUpdates(to: OperationQueue.main) { [weak self] (data: CMMagnetometerData?, error) in
+                // data is CMMagnetometerData?
+                if error != nil {
+                    print("Magnetometer Error: \(error?.localizedDescription ?? "Unknown Error")")
+                    return
+                }
+                guard let magnetometerData = data else {
+                    print("Bad Magnetometer Data!")
+                    return
+                }
+                let magX = magnetometerData.magneticField.x
+                let magY = magnetometerData.magneticField.y
+                let magZ = magnetometerData.magneticField.z
+                print("Magnetometer Update: [x:\(magX), y:\(magY), z:\(magZ)]")
+                self?.updateMagnetometerLabel(magnetometerData)
+            }
+            
+        }
+    }
+    
+    func stopMagnetometer() {
+        MotionTracker.motionManager.stopMagnetometerUpdates()
+    }
+
+
 }
+
+extension Double {
+    var shortValue: String {
+        return String(format: "%.4f", self)
+    }
+}
+
